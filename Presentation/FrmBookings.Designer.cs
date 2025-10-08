720d5e0a1757d114dec83544e786f47895116ab8
Simon <Simon@SBARAKA>
2025-10-08 15:11:43 +0200

    no commits made

--- Presentation/FrmBookings.Designer.cs
+++ Presentation/FrmBookings.Designer.cs
@@ -28,31 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.FrmBookLabel = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label9 = new System.Windows.Forms.Label();
+            this.PhonetextBox = new System.Windows.Forms.TextBox();
+            this.CardtextBox = new System.Windows.Forms.TextBox();
+            this.SecondNametextBox = new System.Windows.Forms.TextBox();
+            this.EmailtextBox = new System.Windows.Forms.TextBox();
+            this.AddresstextBox = new System.Windows.Forms.TextBox();
+            this.FormtextBox = new System.Windows.Forms.TextBox();
+            this.DatetextBox = new System.Windows.Forms.TextBox();
+            this.FirstNametextBox = new System.Windows.Forms.TextBox();
+            this.Formlabel = new System.Windows.Forms.Label();
+            this.Datelabel = new System.Windows.Forms.Label();
+            this.Phonelabel = new System.Windows.Forms.Label();
+            this.Addresslabel = new System.Windows.Forms.Label();
+            this.SecondNamelabel = new System.Windows.Forms.Label();
+            this.Emaillabel = new System.Windows.Forms.Label();
+            this.Cardlabel = new System.Windows.Forms.Label();
+            this.FirstNamelabel = new System.Windows.Forms.Label();
+            this.FrmGuestsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // FrmBookLabel
+            // label9
             // 
-            this.FrmBookLabel.AutoSize = true;
-            this.FrmBookLabel.Font = new System.Drawing.Font("Nirmala UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FrmBookLabel.ForeColor = System.Drawing.Color.White;
-            this.FrmBookLabel.Location = new System.Drawing.Point(298, 151);
-            this.FrmBookLabel.Name = "FrmBookLabel";
-            this.FrmBookLabel.Size = new System.Drawing.Size(361, 50);
-            this.FrmBookLabel.TabIndex = 0;
-            this.FrmBookLabel.Text = "Your Bookings here";
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Nirmala UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(298, 76);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(365, 50);
+            this.label9.TabIndex = 43;
+            this.label9.Text = "MANAGE BOOKING";
             // 
-            // pictureBox1
+            // PhonetextBox
             // 
-            this.pictureBox1.Image = global::Phumla_Kamnandi_GRP_12.Properties.Resources.event_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz241;
-            this.pictureBox1.Location = new System.Drawing.Point(260, 204);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(454, 263);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.PhonetextBox.Location = new System.Drawing.Point(428, 354);
+            this.PhonetextBox.Name = "PhonetextBox";
+            this.PhonetextBox.Size = new System.Drawing.Size(298, 22);
+            this.PhonetextBox.TabIndex = 42;
+            // 
+            // CardtextBox
+            // 
+            this.CardtextBox.Location = new System.Drawing.Point(428, 431);
+            this.CardtextBox.Name = "CardtextBox";
+            this.CardtextBox.Size = new System.Drawing.Size(298, 22);
+            this.CardtextBox.TabIndex = 41;
+            // 
+            // SecondNametextBox
+            // 
+            this.SecondNametextBox.Location = new System.Drawing.Point(428, 266);
+            this.SecondNametextBox.Name = "SecondNametextBox";
+            this.SecondNametextBox.Size = new System.Drawing.Size(298, 22);
+            this.SecondNametextBox.TabIndex = 40;
+            // 
+            // EmailtextBox
+            // 
+            this.EmailtextBox.Location = new System.Drawing.Point(428, 312);
+            this.EmailtextBox.Name = "EmailtextBox";
+            this.EmailtextBox.Size = new System.Drawing.Size(298, 22);
+            this.EmailtextBox.TabIndex = 39;
+            // 
+            // AddresstextBox
+            // 
+            this.AddresstextBox.Location = new System.Drawing.Point(428, 391);
+            this.AddresstextBox.Name = "AddresstextBox";
+            this.AddresstextBox.Size = new System.Drawing.Size(298, 22);
+            this.AddresstextBox.TabIndex = 38;
+            // 
+            // FormtextBox
+            // 
+            this.FormtextBox.Location = new System.Drawing.Point(428, 520);
+            this.FormtextBox.Name = "FormtextBox";
+            this.FormtextBox.Size = new System.Drawing.Size(298, 22);
+            this.FormtextBox.TabIndex = 37;
+            // 
+            // DatetextBox
+            // 
+            this.DatetextBox.Location = new System.Drawing.Point(428, 479);
+            this.DatetextBox.Name = "DatetextBox";
+            this.DatetextBox.Size = new System.Drawing.Size(298, 22);
+            this.DatetextBox.TabIndex = 36;
+            // 
+            // FirstNametextBox
+            // 
+            this.FirstNametextBox.Location = new System.Drawing.Point(428, 219);
+            this.FirstNametextBox.Name = "FirstNametextBox";
+            this.FirstNametextBox.Size = new System.Drawing.Size(298, 22);
+            this.FirstNametextBox.TabIndex = 35;
+            // 
+            // Formlabel
+            // 
+            this.Formlabel.AutoSize = true;
+            this.Formlabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Formlabel.Location = new System.Drawing.Point(153, 523);
+            this.Formlabel.Name = "Formlabel";
+            this.Formlabel.Size = new System.Drawing.Size(114, 16);
+            this.Formlabel.TabIndex = 34;
+            this.Formlabel.Text = "IsInGoodStanding";
+            // 
+            // Datelabel
+            // 
+            this.Datelabel.AutoSize = true;
+            this.Datelabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Datelabel.Location = new System.Drawing.Point(154, 482);
+            this.Datelabel.Name = "Datelabel";
+            this.Datelabel.Size = new System.Drawing.Size(106, 16);
+            this.Datelabel.TabIndex = 33;
+            this.Datelabel.Text = "Date Registered";
+            // 
+            // Phonelabel
+            // 
+            this.Phonelabel.AutoSize = true;
+            this.Phonelabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Phonelabel.Location = new System.Drawing.Point(158, 357);
+            this.Phonelabel.Name = "Phonelabel";
+            this.Phonelabel.Size = new System.Drawing.Size(46, 16);
+            this.Phonelabel.TabIndex = 32;
+            this.Phonelabel.Text = "Phone";
+            // 
+            // Addresslabel
+            // 
+            this.Addresslabel.AutoSize = true;
+            this.Addresslabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Addresslabel.Location = new System.Drawing.Point(153, 397);
+            this.Addresslabel.Name = "Addresslabel";
+            this.Addresslabel.Size = new System.Drawing.Size(58, 16);
+            this.Addresslabel.TabIndex = 31;
+            this.Addresslabel.Text = "Address";
+            // 
+            // SecondNamelabel
+            // 
+            this.SecondNamelabel.AutoSize = true;
+            this.SecondNamelabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.SecondNamelabel.Location = new System.Drawing.Point(153, 270);
+            this.SecondNamelabel.Name = "SecondNamelabel";
+            this.SecondNamelabel.Size = new System.Drawing.Size(97, 16);
+            this.SecondNamelabel.TabIndex = 30;
+            this.SecondNamelabel.Text = "Second  Name";
+            // 
+            // Emaillabel
+            // 
+            this.Emaillabel.AutoSize = true;
+            this.Emaillabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Emaillabel.Location = new System.Drawing.Point(161, 314);
+            this.Emaillabel.Name = "Emaillabel";
+            this.Emaillabel.Size = new System.Drawing.Size(41, 16);
+            this.Emaillabel.TabIndex = 29;
+            this.Emaillabel.Text = "Email";
+            // 
+            // Cardlabel
+            // 
+            this.Cardlabel.AutoSize = true;
+            this.Cardlabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cardlabel.Location = new System.Drawing.Point(153, 437);
+            this.Cardlabel.Name = "Cardlabel";
+            this.Cardlabel.Size = new System.Drawing.Size(128, 16);
+            this.Cardlabel.TabIndex = 28;
+            this.Cardlabel.Text = "Credit Card Number ";
+            // 
+            // FirstNamelabel
+            // 
+            this.FirstNamelabel.AutoSize = true;
+            this.FirstNamelabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FirstNamelabel.Location = new System.Drawing.Point(153, 219);
+            this.FirstNamelabel.Name = "FirstNamelabel";
+            this.FirstNamelabel.Size = new System.Drawing.Size(72, 16);
+            this.FirstNamelabel.TabIndex = 27;
+            this.FirstNamelabel.Text = "First Name";
+            // 
+            // FrmGuestsLabel
+            // 
+            this.FrmGuestsLabel.AutoSize = true;
+            this.FrmGuestsLabel.Font = new System.Drawing.Font("Nirmala UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FrmGuestsLabel.ForeColor = System.Drawing.Color.White;
+            this.FrmGuestsLabel.Location = new System.Drawing.Point(107, 11);
+            this.FrmGuestsLabel.Name = "FrmGuestsLabel";
+            this.FrmGuestsLabel.Size = new System.Drawing.Size(740, 50);
+            this.FrmGuestsLabel.TabIndex = 26;
+            this.FrmGuestsLabel.Text = "PHUMLA KAMNANDI GROUP OF HOTELS";
             // 
             // FrmBookings
             // 
@@ -60,12 +212,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(954, 709);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.FrmBookLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.PhonetextBox);
+            this.Controls.Add(this.CardtextBox);
+            this.Controls.Add(this.SecondNametextBox);
+            this.Controls.Add(this.EmailtextBox);
+            this.Controls.Add(this.AddresstextBox);
+            this.Controls.Add(this.FormtextBox);
+            this.Controls.Add(this.DatetextBox);
+            this.Controls.Add(this.FirstNametextBox);
+            this.Controls.Add(this.Formlabel);
+            this.Controls.Add(this.Datelabel);
+            this.Controls.Add(this.Phonelabel);
+            this.Controls.Add(this.Addresslabel);
+            this.Controls.Add(this.SecondNamelabel);
+            this.Controls.Add(this.Emaillabel);
+            this.Controls.Add(this.Cardlabel);
+            this.Controls.Add(this.FirstNamelabel);
+            this.Controls.Add(this.FrmGuestsLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmBookings";
             this.Text = "FrmBookings";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -73,7 +240,23 @@
 
         #endregion
 
-        private System.Windows.Forms.Label FrmBookLabel;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox PhonetextBox;
+        private System.Windows.Forms.TextBox CardtextBox;
+        private System.Windows.Forms.TextBox SecondNametextBox;
+        private System.Windows.Forms.TextBox EmailtextBox;
+        private System.Windows.Forms.TextBox AddresstextBox;
+        private System.Windows.Forms.TextBox FormtextBox;
+        private System.Windows.Forms.TextBox DatetextBox;
+        private System.Windows.Forms.TextBox FirstNametextBox;
+        private System.Windows.Forms.Label Formlabel;
+        private System.Windows.Forms.Label Datelabel;
+        private System.Windows.Forms.Label Phonelabel;
+        private System.Windows.Forms.Label Addresslabel;
+        private System.Windows.Forms.Label SecondNamelabel;
+        private System.Windows.Forms.Label Emaillabel;
+        private System.Windows.Forms.Label Cardlabel;
+        private System.Windows.Forms.Label FirstNamelabel;
+        private System.Windows.Forms.Label FrmGuestsLabel;
     }
 }
