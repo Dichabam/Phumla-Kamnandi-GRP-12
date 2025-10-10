c864a3d8092363f52d32a3262ed7d1435b3805b4
kagom <kagomotata5@gmail.com>
2025-10-10 16:46:29 +0200

    s

--- Business/Entities/RoomDB.cs
+++ Business/Entities/RoomDB.cs
@@ -15,7 +15,7 @@ namespace Phumla_Kamnandi_GRP_12.Database
         public Room GetByNumber(int roomNumber)
         {
             using var cn = new SqlConnection(connectionString);
-            var cmd = new SqlCommand("SELECT * FROM Room WHERE RoomNumber = @num", cn);
+            var cmd = new SqlCommand("SELECT * FROM Room WHERE RoomNum = @num", cn);
             cmd.Parameters.AddWithValue("@num", roomNumber);
             cn.Open();
             using var reader = cmd.ExecuteReader();
@@ -40,9 +40,9 @@ namespace Phumla_Kamnandi_GRP_12.Database
             using var cn = new SqlConnection(connectionString);
             var cmd = new SqlCommand(@"
                 SELECT r.* FROM Room r
-                WHERE r.IsAvailable = 1 AND NOT EXISTS (
+                WHERE r.Availability = 1 AND NOT EXISTS (
                     SELECT 1 FROM Booking b
-                    WHERE b.RoomNumber = r.RoomNumber
+                    WHERE b.RoomNum = r.RoomNum
                       AND b.Status != 2 -- Exclude cancelled
                       AND @date BETWEEN b.CheckInDate AND b.CheckOutDate
                 )", cn);
@@ -58,7 +58,7 @@ namespace Phumla_Kamnandi_GRP_12.Database
         public void Update(Room room)
         {
             using var cn = new SqlConnection(connectionString);
-            var cmd = new SqlCommand("UPDATE Room SET IsAvailable = @available WHERE RoomNumber = @num", cn);
+            var cmd = new SqlCommand("UPDATE Room SET Availability = @available WHERE RoomNum = @num", cn);
             cmd.Parameters.AddWithValue("@num", room.RoomNumber);
             cmd.Parameters.AddWithValue("@available", room.IsAvailable);
             cn.Open();
@@ -67,8 +67,8 @@ namespace Phumla_Kamnandi_GRP_12.Database
 
         private Room MapRoom(SqlDataReader reader)
         {
-            var room = new Room(Convert.ToInt32(reader["RoomNumber"]));
-            room.IsAvailable = Convert.ToBoolean(reader["IsAvailable"]);
+            var room = new Room(Convert.ToInt32(reader["RoomNum"]));
+            room.IsAvailable = Convert.ToBoolean(reader["Availability"]);
             return room;
         }
     }
